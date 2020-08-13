1df61fea166ebf368a01711246fc4c108558691f
Debian server <you@exapmle.com>
2020-08-13 14:56:19 +0300

    commit

--- internet_magazin/main/models.py
+++ internet_magazin/main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Comment(models.Model):
-    title = models.CharField(max_lenght=100)
-    comment = models.TextField()
-    name_user = models.DateTimeField()
+#class Comment(models.Model):
+#    title = models.CharField(max_lenght=100)
+#    comment = models.TextField()
+#    name_user = models.DateTimeField()
 
-    def __str__(self):
-        return self.title
+#    def __str__(self):
+#        return self.title
